4b3ef2942f93eb7540a4a14f284d2fb3322ce3d8
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-02-01 19:37:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,14 +10,24 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-let price = Math.floor(Math.random() * 100) + 1
+let price = Math.floor(Math.random() * 100) + 1;
   var item = {itemName: name, itemPrice: price };
   cart.push(item);
   return `${item["itemName"]} has been added to your cart.`
 }
 
 function viewCart() {
-  // write your code here
+  if (cart === undefined || cart.length == 0) {
+    return "Your shopping cart is empty.";
+  } else {
+    if (cart.length==1) {
+      return `In your cart, you have ${item["itemName"]} at $${item["itemPrice"]}.`
+    } else {
+      for (var i = 0; i < cart.length; i++) {
+      return `In your cart, you have, ${item["itemName"]} at $${item["itemPrice"]}, and`;
+    }
+}
+}
 }
 
 function total() {
