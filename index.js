a1143b61349bd7252f03fe2d7341329263c7a62e
Elizabeth Thys <elizabeth.thys@gmail.com>
2019-02-01 20:02:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,33 +9,30 @@ function setCart(c) {
   return cart;
 }
 
-function addToCart(name) {
+function addToCart(item) {
 let price = Math.floor(Math.random() * 100) + 1;
-  var item = {itemName: name, itemPrice: price };
-  cart.push(item);
-  return `${item["itemName"]} has been added to your cart.`
+  var newitem = {itemName: name, itemPrice: price };
+  cart.push(newitem);
+  return `${newitem["itemName"]} has been added to your cart.`
 }
 
 function viewCart() {
-  let list = []
+  let list = [];
   let last = "and";
   if (cart === undefined || cart.length == 0) {
     return "Your shopping cart is empty.";
   } else {
     if (cart.length===1) {
-      return `In your cart, you have ${item[0]["itemName"]} at $${item[0]["itemPrice"]}.`
+      return `In your cart, you have ${cart[0]["itemName"]} at $${cart[0]["itemPrice"]}.`
     } else {
-      for (var i = 0; i < cart.length; i++) {
-        if (i === cart.length -1) {
-          last = `, and ${cart[i]["itemName"]} at $${cart[i]["itemPrice"]}.`
-        } else {
-          list_of_items = ` ${cart[i]["itemName"]} at $${cart[i]["itemPrice"]}`;
-          list.push(list_of_items)
+      var thing = "In your cart, you have ";
+    for (var i = 0; i < cart.length -1; i++) {
+      thing += `${cart[i].itemName} at $${cart[i].itemPrice}, `;
+  }
+    thing += `and ${cart[cart.length -1].itemName} at $${cart[cart.length -1].itemPrice}.`
           }
-      return "In your cart, you have, " + list + last;
+      return thing
     }
-}
-}
 }
 
 function total() {
